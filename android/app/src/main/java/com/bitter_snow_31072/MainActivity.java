d88b2fa1295ee240abf1981c3449cd93a143f72f
Crowdbotics <team@crowdbotics.com>
2021-10-03 17:52:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bitter_snow_31072/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bitter_snow_31072;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bitter_snow_31072";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bitter_snow_31072",
+  url: "https://bitter_snow_31072.botics.co",
+  api: "https://bitter_snow_31072.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
